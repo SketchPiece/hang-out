054b453b877b6a8bef487af49b264485ef445312
Sketch Piece <lubkin.andrey@gmail.com>
2022-12-12 17:51:17 +0100

    fix port

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import http from 'http'
 import { createSocketServer } from './socket'
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
 
